7103726075ef869e2ec4dbd39d3d025f176c1ed6
Thomas Edgar <tom@Thomass-MacBook-Pro.local>
2025-07-21 18:11:15 +0100

    Add Cloudflare configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  adapter: cloudflare()
 });
