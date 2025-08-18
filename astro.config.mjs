a55c1bbc6b45056ea36269ebe65e9fe92a0d5945
Simon Richards <sjrichards88@gmail.com>
2025-08-18 12:19:50 +0100

    alot of fixes and adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,35 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({
-    mode: 'directory',
-    functionPerRoute: true,
-    routes: {
-      strategy: 'include',
-      include: ['/api/create-checkout-session']
-    },
-    imageService: 'cloudflare'
-  }),
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/sharp',
-      config: {
-        quality: 80
-      }
-    },
-    domains: ['i.ytimg.com'], // Allow YouTube thumbnails
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.gstatic.com'
-      }
-    ]
-  },
+  adapter: cloudflare(),
+  integrations: [sitemap()],
   vite: {
+    plugins: [tailwindcss()],
     build: {
-      cssCodeSplit: false,
-      rollupOptions: {
-        output: {
-          manualChunks: undefined
-        }
-      }
+      sourcemap: true
+    },
+    css: {
+      devSourcemap: true
     }
-  }
+  },
+  // image: {
+  //   service: {
+  //     entrypoint: 'astro/assets/services/sharp',
+  //     config: {
+  //       quality: 80
+  //     }
+  //   },
+  //   domains: ['i.ytimg.com'],
+  //   // Allow YouTube thumbnails
+  //   remotePatterns: [{
+  //     protocol: 'https',
+  //     hostname: '**.gstatic.com'
+  //   }]
+  // }
 });
