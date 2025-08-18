086b9cfc139535a1107dbceac0daede84e309c49
Simon Richards <sjrichards88@gmail.com>
2025-08-18 12:33:56 +0100

    sitemap fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.coachedgar.com',
   output: 'server',
   adapter: cloudflare(),
   integrations: [sitemap()],
